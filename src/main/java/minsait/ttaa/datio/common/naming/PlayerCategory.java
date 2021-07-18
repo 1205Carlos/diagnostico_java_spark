7957bbdc9965d411bc6df215a50ad4a0f1a3b0bd
Carlos Lopez <clopezr1205@hotmail.com>
2021-07-18 17:57:10 -0500

    feat(diagnostic): finish diagnostic test

---
+++ src/main/java/minsait/ttaa/datio/common/naming/PlayerCategory.java
@@ -0,0 +1,8 @@
+package minsait.ttaa.datio.common.naming;
+
+public final class PlayerCategory {
+	public static String CAT_A = "A";
+	public static String CAT_B = "B";
+	public static String CAT_C = "C";
+	public static String CAT_D = "D";
+}
--- src/main/java/minsait/ttaa/datio/engine/Transformer.java
+++ src/main/java/minsait/ttaa/datio/engine/Transformer.java
@@ -1,5 +1,38 @@
 package minsait.ttaa.datio.engine;
 
+import static minsait.ttaa.datio.common.Common.HEADER;
+import static minsait.ttaa.datio.common.Common.INFER_SCHEMA;
+import static minsait.ttaa.datio.common.Common.INPUT_PATH;
+import static minsait.ttaa.datio.common.Common.PARAMS_PATH;
+import static minsait.ttaa.datio.common.Common.PARAM_WORK_DS;
+import static minsait.ttaa.datio.common.naming.PlayerCategory.CAT_A;
+import static minsait.ttaa.datio.common.naming.PlayerCategory.CAT_B;
+import static minsait.ttaa.datio.common.naming.PlayerCategory.CAT_C;
+import static minsait.ttaa.datio.common.naming.PlayerCategory.CAT_D;
+import static minsait.ttaa.datio.common.naming.PlayerInput.age;
+import static minsait.ttaa.datio.common.naming.PlayerInput.clubName;
+import static minsait.ttaa.datio.common.naming.PlayerInput.heightCm;
+import static minsait.ttaa.datio.common.naming.PlayerInput.longName;
+import static minsait.ttaa.datio.common.naming.PlayerInput.nationality;
+import static minsait.ttaa.datio.common.naming.PlayerInput.overall;
+import static minsait.ttaa.datio.common.naming.PlayerInput.potential;
+import static minsait.ttaa.datio.common.naming.PlayerInput.shortName;
+import static minsait.ttaa.datio.common.naming.PlayerInput.teamPosition;
+import static minsait.ttaa.datio.common.naming.PlayerInput.weight_kg;
+import static minsait.ttaa.datio.common.naming.PlayerOutput.catHeightByPosition;
+import static minsait.ttaa.datio.common.naming.PlayerOutput.playerCat;
+import static minsait.ttaa.datio.common.naming.PlayerOutput.potentialVsOverall;
+import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.rank;
+import static org.apache.spark.sql.functions.when;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.spark.api.java.function.ForeachFunction;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -7,13 +40,12 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.expressions.Window;
 import org.apache.spark.sql.expressions.WindowSpec;
 import org.jetbrains.annotations.NotNull;
-
-import static minsait.ttaa.datio.common.Common.*;
-import static minsait.ttaa.datio.common.naming.PlayerInput.*;
-import static minsait.ttaa.datio.common.naming.PlayerOutput.*;
-import static org.apache.spark.sql.functions.*;
+import org.jetbrains.annotations.TestOnly;
 
 public class Transformer extends Writer {
+		private final static double MIN_POT_OVER_C = 1.15;
+		private final static double MIN_POT_OVER_D = 1.25;
+	
     private SparkSession spark;
 
     public Transformer(@NotNull SparkSession spark) {
@@ -23,24 +55,50 @@ public class Transformer extends Writer {
         df.printSchema();
 
         df = cleanData(df);
-        df = exampleWindowFunction(df);
         df = columnSelection(df);
+        
+        String param23Years = getParameterFile(PARAM_WORK_DS);
+        
+        //Ejercicio 5
+        df = apply23YearsFilterDataset(df, Integer.parseInt(param23Years));
 
-        // for show 100 records after your transformations and show the Dataset schema
-        df.show(100, false);
-        df.printSchema();
-
+        //Ejercicio 2
+        df = playerCatFunction(df);
+        
+        //Ejercicio 3
+        df = potentialVsOverallFunction(df);
+        
+        //Ejercicio 4
+        df = filterByCatAndPotentialOverallFunction(df);
+        
+        df.show(100,false);
+        
+        
+        //Test ejercicio 4
+        try {
+        	testFilterByCatAndPotentialOverallFunction(df);
+        }catch(Exception e) {
+        	return;
+        }
+        
         // Uncomment when you want write your final output
-        //write(df);
+        //Ejercicio 6
+        write(df);
     }
 
     private Dataset<Row> columnSelection(Dataset<Row> df) {
+    	//Ejercicio 1
         return df.select(
                 shortName.column(),
-                overall.column(),
+                longName.column(),
+                age.column(),
                 heightCm.column(),
-                teamPosition.column(),
-                catHeightByPosition.column()
+                weight_kg.column(),
+                nationality.column(),
+                clubName.column(),
+                overall.column(),
+                potential.column(),
+                teamPosition.column()
         );
     }
 
@@ -53,6 +111,29 @@ public class Transformer extends Writer {
                 .option(INFER_SCHEMA, true)
                 .csv(INPUT_PATH);
         return df;
+    }
+    
+    private String getParameterFile(String paramName) {
+    	try(Stream<String> stream = Files.lines(Paths.get(PARAMS_PATH))) {
+    		String value = stream
+    				.filter(line -> line.startsWith(paramName))
+    				.collect(Collectors.toList())
+    				.get(0);
+    		
+    		value = value.substring(paramName.length() + 1);
+    		return value;
+    	}catch(IOException e) {
+    		e.printStackTrace();
+    		return null;
+    	}
+    }
+    
+    private Dataset<Row> apply23YearsFilterDataset(Dataset<Row> df, int param) {
+    	if(param == 1) {
+    		return df.filter(age.column().lt(23));
+    	}
+    	
+    	return df;
     }
 
     /**
@@ -95,8 +176,74 @@ public class Transformer extends Writer {
 
         return df;
     }
-
-
-
-
+    
+    private Dataset<Row> playerCatFunction(Dataset<Row> df){
+    	WindowSpec w = Window
+    					.partitionBy(nationality.column(), teamPosition.column())
+    					.orderBy(overall.column().desc());
+    	
+    	Column rank = rank().over(w);
+    	Column rule = when(rank.$less(3), CAT_A)
+    							.when(rank.$less(5), CAT_B)
+    							.when(rank.$less(10), CAT_C)
+    							.otherwise(CAT_D);
+    	
+    	df = df.withColumn(playerCat.getName(), rule);
+    	return df;
+    }
+    
+    private Dataset<Row> potentialVsOverallFunction(Dataset<Row> df){
+    	Column rule = col(potential.getName())
+    									.divide(col(overall.getName()));
+    	
+    	df = df.withColumn(potentialVsOverall.getName(), rule);
+    	return df;
+    }
+    
+    private Dataset<Row> filterByCatAndPotentialOverallFunction(Dataset<Row> df){
+    	Column ruleA = col(playerCat.getName())
+    			.equalTo("A");
+    	
+    	Column ruleB = col(playerCat.getName())
+    			.equalTo("B");
+    	
+    	//Rules for A and B
+    	Column ruleAB = ruleA.or(ruleB);
+    	
+    	Column ruleC = col(playerCat.getName()).equalTo("C")
+    			.and(col(potentialVsOverall.getName())
+    			.gt(MIN_POT_OVER_C));
+    	
+    	Column ruleD = col(playerCat.getName()).equalTo("D")
+    			.and(col(potentialVsOverall.getName())
+    			.gt(MIN_POT_OVER_D));	
+    	
+    	//Rules for C and D
+    	Column ruleCD = ruleC.or(ruleD);
+    	
+    	//Join all rules
+    	Column rules = ruleAB.or(ruleCD);
+    	
+    	df = df.filter(rules);
+    	return df;
+    }
+    
+    @TestOnly
+    void testFilterByCatAndPotentialOverallFunction(Dataset<Row> df) throws Exception{
+    	
+    	df.foreach((ForeachFunction<Row>) s -> {
+    		String playerCatRow = s.getAs(playerCat.getName());
+    		double potOver = s.getAs(potentialVsOverall.getName());
+    		
+    		if(playerCatRow.equals(CAT_C)) {
+    			if(potOver < MIN_POT_OVER_C) {
+    				throw new Exception();
+    			}
+    		}else if(playerCatRow.equals(CAT_D)) {
+    			if(potOver < MIN_POT_OVER_D) {
+    				throw new Exception();
+    			}
+    		}
+    	});
+    }
 }
